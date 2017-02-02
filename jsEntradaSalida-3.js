23829667ae0f88ee103773871968112996a8d1cb
aaro16 <abbulrodriguez@hotmail.com>
2017-02-02 09:53:21 -0300

    ejercicio 3

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,15 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	
+var nombre ;
+nombre = document.getElementById('elNombre').value;
+
+alert("el nombre ingresado es: " +nombre) ;
+
+
+
+}
+
+
