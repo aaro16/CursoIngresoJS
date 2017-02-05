47adf89970515774633853c1aa5b5b4ae6127dc4
aaro16 <abbulrodriguez@hotmail.com>
2017-02-05 09:41:00 -0300

    ejercicio 2 terminado

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,15 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var Edad
+
+
+    Edad = prompt("Ingrese su Edad: ") ;
+ 
+ alert("Su edad es: " +Edad ) ;
+
+
+
+}
+
