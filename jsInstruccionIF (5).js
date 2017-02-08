48e16a772da6601720327edb11bf8941fcb58f8a
aaro16 <abbulrodriguez@hotmail.com>
2017-02-08 10:30:34 -0300

    IF ejercicio 5

---
+++ jsInstruccionIF (5).js
@@ -0,0 +1,16 @@
+function Mostrar()
+{
+//tomo la edad  
+
+var edad
+
+edad=document.getElementById('edad').value;
+edad=parseInt(edad);
+
+if(edad<13 || edad>17) 
+{
+
+	alert("No es un adolescente");
+}
+
+}//FIN DE LA FUNCIÓN
