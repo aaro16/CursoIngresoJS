48260771658916c70d38e3ff86b2de0059113e80
aaro16 <abbulrodriguez@hotmail.com>
2017-02-08 10:24:12 -0300

    IF ejercicio 4

---
+++ jsInstruccionIF (4).js
@@ -0,0 +1,20 @@
+function Mostrar()
+{
+//tomo la edad  
+
+var edad
+edad =document.getElementById('edad').value;
+edad=parseInt(edad);
+
+if(edad>=13 && edad<=17)
+
+{
+	alert("Es un adolescente");
+}
+
+
+/* operadores logicos: y(&&)  o(||)  no(!)
+*/
+
+
+}//FIN DE LA FUNCIÓN
