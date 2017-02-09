57fa205ec920261af5ff755ca30d19c04a9bdd5c
aaro16 <abbulrodriguez@hotmail.com>
2017-02-09 12:15:06 -0300

    Switch, ejercicio 5

---
+++ jsInstruccionSwitch(5).js
@@ -0,0 +1,25 @@
+function Mostrar()
+{
+//tomo la edad  
+var laHora = document.getElementById('hora').value;
+
+//alert (laHora);
+	
+switch(laHora)
+{
+	case"7":
+	case"8":
+	case"9":
+	case"10":
+	case"11":
+	alert("Es de mañana");
+	break;
+
+/*no puedo poner mayor, menor, o igual. no puedo
+usar esas funciones, solo con el IF*/
+
+}
+
+
+
+}//FIN DE LA FUNCIÓN
