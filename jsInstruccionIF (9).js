772d71cb2bfd21a7a9e96e9a836e25b07309fb8d
aaro16 <abbulrodriguez@hotmail.com>
2017-02-09 09:53:43 -0300

    if ejercicio 9

---
+++ jsInstruccionIF (9).js
@@ -0,0 +1,12 @@
+function Mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+	
+var numeroRandom = Math.floor((Math.random() * 10) + 1);
+
+
+alert("Numero " +numeroRandom);	
+
+
+
+}//FIN DE LA FUNCIÓN
