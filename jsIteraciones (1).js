aaa7e84ce87183e4a48b3e26a683c4674a4878e1
aaro16 <abbulrodriguez@hotmail.com>
2017-02-12 13:42:29 -0300

    WHILE, ejercicio 1

---
+++ jsIteraciones (1).js
@@ -0,0 +1,20 @@
+function Mostrar()
+{
+
+var numeroinicial=1
+
+while(numeroinicial<11)
+{
+	alert("Numero " +numeroinicial);
+	numeroinicial=numeroinicial+1;
+}
+
+/*el "WHILE" tampoco lleva ";" , para que se
+ejecute el "WHILE" tengo que ir sumandole el valor
+de la misma variable mas "1" , y poner el tope 
+de la suma para que se deje de ejecutar*/
+
+//	alert('iteración while');
+
+
+}//FIN DE LA FUNCIÓN
