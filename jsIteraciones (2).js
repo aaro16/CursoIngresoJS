4ddeb8c8877c20430d1e312b2d26bf63d9d4a346
aaro16 <abbulrodriguez@hotmail.com>
2017-02-12 13:47:27 -0300

    WHILE, ejercicio 2

---
+++ jsIteraciones (2).js
@@ -0,0 +1,17 @@
+function Mostrar()
+{
+	alert('iteración while');
+
+var numeroCambio=10;
+
+while(numeroCambio>=0)
+{
+	alert("Numero " +numeroCambio);
+	numeroCambio=numeroCambio-1;
+	
+}
+
+
+
+
+}//FIN DE LA FUNCIÓN
