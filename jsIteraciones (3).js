6f208d28e7c54dd8abf7deba940e94bac6cac4a0
aaro16 <abbulrodriguez@hotmail.com>
2017-02-15 09:53:46 -0300

    WHILE, ejercicio 3 terminado!!

--- jsIteraciones (3).js
+++ jsIteraciones (3).js
@@ -1,18 +1,19 @@
 function Mostrar()
 {
 
-
+var cantidadDeIntentos=1;
 var clave = prompt("ingrese el número clave.");
 
-
-while(clave=="utn750")
+while(clave!=="utn750" && cantidadDeIntentos<3)
 
 {
-	alert("Clave Correcta");
+	clave=prompt("ingrese el número clave.");
+	cantidadDeIntentos=cantidadDeIntentos+1;
+
+/*se restringio la cantidad de intentos, se puso 
+maximo 3 intentos. */
+
 
 }
-
-
-
 
 }//FIN DE LA FUNCIÓN
